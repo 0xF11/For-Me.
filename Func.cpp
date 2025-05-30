a8cfd653b9d7723856afc2eb957711e61859212b
0xF11 <140972657+0xF11@users.noreply.github.com>
2025-05-30 13:17:42 -0700

    Create Func.cpp

---
+++ Func.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void textel(std::string mg) // print whatever we want w/ new line
+{
+	std::cout << mg << std::endl;
+}
+void text(std::string mgn)
+{
+	std::cout << mgn; // print what we want, no newline.
+}
